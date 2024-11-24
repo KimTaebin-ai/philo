9e41712d169da7075e5661e45eff9d724091ed54
Taebin Kim <taebkim@c2r8s1.42gyeongsan.kr>
2024-11-24 14:41:41 +0900

    test

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: taebkim <taebkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 21:16:56 by taebkim           #+#    #+#             */
-/*   Updated: 2024/11/24 04:10:25 by taebkim          ###   ########.fr       */
+/*   Updated: 2024/11/24 11:04:07 by taebkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,31 +22,31 @@
 typedef struct s_philo
 {
 	/* data */
-	pthread_t	philosopher;
+	pthread_t		philosopher;
 	/*
 		thread
 	*/
-	int			id;
+	int				id;
 	/*
 		철학자 unique id
 	*/
-	int			left_fork;
-	int			right_fork;
-	int			eat_count;
-	long		last_eat;
-}				t_philo;
+	int				left_fork;
+	int				right_fork;
+	int				eat_count;
+	long			last_eat;
+}					t_philo;
 
 typedef struct s_rules
 {
 	/* data */
 
-	int			number_of_philosophers;
+	int				number_of_philosophers;
 	/*
 		철학자의 수 :
 		철학자의 수와 포크의 수입니다.
 	*/
 
-	int			time_to_die;
+	int				time_to_die;
 	/*
 		철학자의 수명(밀리초 단위) :
 		철학자가 마지막으로 밥을 먹기 시작한 시점으로부터
@@ -55,20 +55,20 @@ typedef struct s_rules
 		식사를 시작하지 않으면 해당 철학자는 사망합니다.
 	*/
 
-	int			time_to_eat;
+	int				time_to_eat;
 	/*
 		밥을 먹는데 걸리는 시간(밀리초 단위) :
 		철학자가 밥을 먹는데 걸리는 시간입니다.
 		해당 시간동안 철학자는 두 개의 포크를 잡고 있어야 합니다.
 	*/
 
-	int			time_to_sleep;
+	int				time_to_sleep;
 	/*
 		잠자는 시간(밀리초 단위) :
 		잠을 자는 데 소모되는 시간 입니다.
 	*/
 
-	int			number_of_times_each_philosopher_must_eat;
+	int				number_of_times_each_philosopher_must_eat;
 	/*
 		각 철학자가 최소한 밥을 먹어야 하는 횟수(선택적 인자):
 		모든 철학자가 number_of_times_each_philosopher_must_eat 번 이상
@@ -76,22 +76,25 @@ typedef struct s_rules
 		지정되지 않은 경우, 철학자가 죽을 때 시뮬레이션이 종료됩니다.
 	*/
 
-}				t_rules;
+}					t_rules;
 
 typedef struct s_table
 {
-	t_rules		*rule;
-	long		time;
-	t_philo		**link_philo;
-}				t_table;
+	t_rules			*rule;
+	long			time;
+	t_philo			**link_philo;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t print;    // printf
+	pthread_mutex_t last_ate; // 공유자원
+}					t_table;
 
-void			error(char *msg);
+void				error(char *msg);
 
 // utils
-int				ft_atoi(const char *nptr);
-long			get_time(long set_time);
+int					ft_atoi(const char *nptr);
+long				get_time(long set_time);
 
 // is_ utils
-int				is_arg_valid(int number, int die, int eat, int sleep);
+int					is_arg_valid(int number, int die, int eat, int sleep);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: taebkim <taebkim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 21:21:03 by taebkim           #+#    #+#             */
-/*   Updated: 2024/11/24 04:19:43 by taebkim          ###   ########.fr       */
+/*   Updated: 2024/11/24 11:26:46 by taebkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@
  */
 void	set_rules(int argc, char **argv, t_table *table)
 {
+	table->rule = malloc(sizeof(t_rules)); // 동적할당 필요
 	table->rule->number_of_philosophers = ft_atoi(argv[1]);
 	table->rule->time_to_die = ft_atoi(argv[2]);
 	table->rule->time_to_eat = ft_atoi(argv[3]);
@@ -48,23 +49,53 @@ int	init_rules(int argc, char **argv, t_table *table)
 	table->time = get_time(0);
 	table->link_philo = malloc(sizeof(t_table *)
 			* table->rule->number_of_philosophers);
-	/*
-		철학자 수만큼 동적할당
-	*/
+	// 철학자 수(number_of_philosophers)만큼 동적할당
 	return (0);
+}
+
+/**
+ * 공유자원인 포크의 개수만큼 mutex 생성
+ */
+void	init_mutex(t_table *table)
+{
+	int	i;
+
+	i = 0;
+	table->fork = malloc(sizeof(pthread_mutex_t)
+			* table->rule->number_of_philosophers);
+	while (i < table->rule->number_of_philosophers)
+	{
+		pthread_mutex_init(&table->fork[i], NULL);
+		i++;
+	}
+	pthread_mutex_init(&table->print, NULL);
+	/*
+		printf() 함수는 버퍼에 쌓여있는 입력값들을 개행문자 '\n' 가 나오면
+		버퍼를 비워주며 출력하는 함수이다.
+		여러 쓰레드가 동시에 printf() 를 호출하게 될 때
+		출력 메시지가 중첩되거나 엉킬 수 있다
+		따라서 뮤텍스를 활용해 한 번에 하나의 쓰레드만을 사용함을 "보장"하고
+		출력 결과를 예상 가능한 형태로 만들어준다.
+	*/
+	pthread_mutex_init(&table->last_ate, NULL);
+	printf("%d", i);
 }
 
 int	main(int argc, char **argv)
 {
 	t_table	table;
 
-	/*
-		각 철학자는 1부터 number_of_philosophers까지의 고유 번호를 가지고 있습니다.
-	*/
-	/*
-		철학자 번호 1은 철학자 번호 number_of_philosophers의 옆에 앉습니다.
-		그 외에 철학자 번호 N 은 철학자 번호 N - 1 과 철학자 번호 N + 1 사이에 앉습니다.
-	*/
+	/**
+		* 각 철학자는 스레드로 구현되어 있어야 합니다.
+		*
+		* 두 철학자 사이에 한 개의 포크가 존재하므로, 철학자가 여러명일 경우
+		* 각 철학자의 왼쪽과 오른쪽에 포크가 하나씩 존재해야 합니다.
+		* 철학자가 한 명일 경우 테이블 위에 포크가 하나만 존재해야 합니다.
+		*
+		* 철학자가 포크를 복제하는 것을 막기 위해서,
+		* 각 포크의 현재 상태를 뮤텍스를 이용하여 보호해주어야 합니다.
+		*
+		*/
 	if (!(argc == 5 || argc == 6))
 	{
 		error("invalid argument");
@@ -72,5 +103,6 @@ int	main(int argc, char **argv)
 	}
 	if (init_rules(argc, argv, &table))
 		return (0);
+	init_mutex(&table);
 	return (0);
 }
